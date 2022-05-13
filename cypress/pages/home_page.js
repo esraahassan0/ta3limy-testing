f2b08da4a1f9e990f22a68d1575cd4d83b84a919
esraaHassan <esraa.hassan0111@gmail.com>
2022-05-14 01:59:03 +0200

    add some comments on home_page.js

--- cypress/pages/home_page.js
+++ cypress/pages/home_page.js
@@ -1,12 +1,21 @@
 export class HomePage {
 
+     /**
+     * launching the website
+     */
     navigate(){
         cy.visit('https://ta3limy.com/')
     }
 
+     /**
+     * clicking the about icon 
+     */
     clickAboutIcon(){
         cy.get('#help-menu > svg').click()
     }
+     /**
+     * selecting faqs icon from the menu
+     */
     selectFaqs(){
         cy.get('[data-cy="faq-menuItem"]').click()
     }
